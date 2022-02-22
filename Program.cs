f89b609398a4fe81f2e1d642d5f19818cd55eeb8
1SurajDesai <surajdesai219@gmail.com>
2022-02-22 23:01:33 +0530

    Updating Program.cs file

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace SnakeAndLadderProgram
         {
             //SinglePlayerAtPosition0.SinglePlayer();
             //RollTheDice.RollDice();
-            CheckOptions.CheckPositionOfPlayer();
-            //WinningPosition100.RepeatTillPlayerReaches100();
+            //CheckOptions.CheckPositionOfPlayer();
+            WinningPosition100.RepeatTillPlayerReaches100();
             //ExactPosition_100.CheckExactPositionOf_100();
             //CalNumberOfDice.CalNoOfDiceThrownToWin();
         }
