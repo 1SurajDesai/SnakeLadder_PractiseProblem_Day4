dbad2dbd1af9544f5ce161a3c74124a20a6ae194
1SurajDesai <surajdesai219@gmail.com>
2022-02-22 22:40:58 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SnakeAndLadderProgram
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SinglePlayerAtPosition0.SinglePlayer();
+            //RollTheDice.RollDice();
+            //CheckOptions.CheckPositionOfPlayer();
+            //WinningPosition100.RepeatTillPlayerReaches100();
+            //ExactPosition_100.CheckExactPositionOf_100();
+            //CalNumberOfDice.CalNoOfDiceThrownToWin();
+        }
+    }
+}
