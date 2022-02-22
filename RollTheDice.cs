f47371509cbdaa46e584db6966dfa8018039be72
1SurajDesai <surajdesai219@gmail.com>
2022-02-22 22:51:26 +0530

    Adding program to to roll the dice

---
+++ RollTheDice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public  class RollTheDice
+    {
+        public static void RollDice()
+        {
+            Console.WriteLine("After rolloung the dice, player gets below number:");
+            Random random = new Random();
+            int num = random.Next(1, 7);
+            Console.WriteLine(num);
+        }
+    }
+}
