d0b2f0762b58387156316bf42c6e9e75a36007eb
1SurajDesai <surajdesai219@gmail.com>
2022-02-22 22:49:27 +0530

    Adding program in which single player at zero position

---
+++ SinglePlayerAtPosition0.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public class SinglePlayerAtPosition0
+    {
+        public static void SinglePlayer()
+        {
+            int position = 0;
+            Console.WriteLine("The single player is at {0} position.", position);
+        }
+    }
+}
