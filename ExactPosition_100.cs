3f769db1cef0fa811b022305802b3ae34c524693
1SurajDesai <surajdesai219@gmail.com>
2022-02-22 23:03:25 +0530

    Adding program to get exact winning position of 100

---
+++ ExactPosition_100.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public class ExactPosition_100
+    {
+        public static void CheckExactPositionOf_100()
+        {
+            int IsLadder = 1, IsSnake = 2, Position = 0;
+
+            while (Position != 100)
+            {
+                // Use of Random function to get one option
+                Random rand = new Random();
+                int option = rand.Next(3);
+                // Use of Random function to roll the dice
+                Random rand2 = new Random();
+                int DiceNum = rand2.Next(1, 7);
+
+                switch (option)
+                {
+                    case 1:
+                        Position += DiceNum;
+                        break;
+                    case 2:
+                        Position -= DiceNum;
+                        break;
+                    default:
+                        Position = Position;
+                        break;
+
+                }
+                if (Position < 0)
+                    Position = 0;
+                if (Position > 100)
+                    Position -= DiceNum;
+
+            }
+            Console.WriteLine("Exact Position of Player For wining is " + Position);
+        }
+    }
+}
